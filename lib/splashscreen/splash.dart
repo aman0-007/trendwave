18f22f6d17fef4f479174014df6d57dfdd2fbbca
aman0-007 <dwivedi.aman2811@gmail.com>
2024-11-05 18:16:57 +0530

    first commit

--- lib/splashscreen/splash.dart
+++ lib/splashscreen/splash.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -12,7 +11,7 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
     super.initState();
 
     // Navigate to the next screen after 3 seconds
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 4), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => NextPage()),
       );
@@ -27,28 +26,12 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              width: 100, // Set your desired width
-              height: 100, // Set your desired height
+              width: 150, // Set your desired width
+              height: 150, // Set your desired height
               child: Image.asset(
-                'assets/app/logogif.gif',
+                'assets/app/TrendWaveFinalSplash.gif',
                 fit: BoxFit.cover,
               ),
-            ),
-            const SizedBox(height: 20), // Add space between GIF and text
-            AnimatedTextKit(
-              animatedTexts: [
-                FadeAnimatedText(
-                  'TrendWave',
-                  duration: const Duration(seconds: 2),
-                  textStyle: const TextStyle(
-                    fontSize: 20.0, // Set the desired font size
-                    fontWeight: FontWeight.w700, // Bold
-                    color: Color(0xFF2D2D2D), // Set the desired color
-                    fontFamily: 'CodecPro', // Use CodecPro font
-                  ),
-                ),
-              ],
-              isRepeatingAnimation: false,
             ),
           ],
         ),
